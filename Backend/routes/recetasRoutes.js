231c749b3cdbbf2975d1fc247db29cfef1326d17
PirrY <danielgiraldoval@gmail.com>
2025-08-22 18:41:31 -0500

    Clean start

--- Backend/routes/recetasRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { getRecetasByPaciente } from '../controllers/recetasController.js';
-
-const router = express.Router();
-
-router.get('/:id', getRecetasByPaciente);
-
-export default router;
